bd0c0a6632b2ed77038e7237785bcfebbae58057
Chirag Jain <cjain8060@gmail.com>
2020-09-27 13:29:41 +0530

    initial setup

---
+++ src/components/PostList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// we created a class based component because we wanted to make sure that we have access to life cycle methods in order to do data loading with redux
+class PostList extends React.Component {
+	render() {
+		return <div>PostList</div>;
+	}
+}
+
+export default PostList;
