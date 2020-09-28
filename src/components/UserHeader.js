e379c010fb950feb3482cbd17db4fb1459cc92ef
Chirag Jain <cjain8060@gmail.com>
2020-09-28 22:27:08 +0530

    extracting logic to mapStateToProps from userHeader.js

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -9,8 +9,7 @@ class UserHeader extends React.Component {
 	}
 
 	render() {
-		const user = this.props.users.find(user => user.id === this.props.userId);
-
+		const { user } = this.props;
 		if (!user) {
 			return null;
 		}
@@ -19,8 +18,10 @@ class UserHeader extends React.Component {
 	}
 }
 
-const mapStateToProps = state => {
-	return { users: state.users };
+// in order to increase reusability we generally extract all the logic like here getting the current user to a seperate file in order to be able to reuse the mapStateToProps function so we extract all the logic in here and declare it in a seperate file
+const mapStateToProps = (state, ownProps) => {
+	// we can access the props part of the userHeader component using the ownProps argument that is available on mapStateToProps
+	return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
